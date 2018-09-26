2e47e102c4558c433e25e1116478cf1e7a1e39dd
Eric Lehmann <coding@kernschall.de>
2018-09-26 13:13:28 +0200

    Enables Fullscreen - spatialises button sound - db to lin conversion

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // use strict; 
 
 // setup babylon
-
 var canvas = document.getElementById('babylon-canvas');
 var container =  document.getElementById('babylon');
 container.style.width= `${window.innerWidth}`;
@@ -9,6 +8,8 @@ container.style.height= `${window.innerHeight}`;
 var engine = new BABYLON.Engine(canvas, true);
 var scene = new BABYLON.Scene(engine);
 var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
+camera.lowerRadiusLimit = 8;
+camera.upperRadiusLimit = 30;
 var anchor = new BABYLON.TransformNode("");
 
 camera.wheelDeltaPercentage = 0.01;
@@ -16,45 +17,23 @@ camera.wheelDeltaPercentage = 0.01;
 camera.attachControl(canvas, true);
 
 var manager = new BABYLON.GUI.GUI3DManager(scene);
-var assetsManager = new BABYLON.AssetsManager(scene);
-// var dome = new BABYLON.VideoDome(
-//   "testdome",
-//   ["./assets/video/VID_20180821_233000_ok_014_exerpt.mp4"],
-//   {
-//     resolution: 32,
-//     clickToPlay: true,
-//     loop: true,
-//     size: 100,
-//   },
-//   scene
-// );
+// TODO: implement assetsManager properly
+// var assetsManager = new BABYLON.AssetsManager(scene);
 
-// Sounds
-var atmoPaths = {
-  at00: "./assets/audio/180507_Brodowin_Test_Huegel_5pm45_cut_stereo.ogg",
-  bell: "./assets/audio/Agogo\ Bell.wav"
-}
+// TODO: How to mute audio of video? >> demux
+var dome = new BABYLON.VideoDome(
+  "testdome",
+  ["./assets/video/VID_20180821_233000_ok_014_exerpt.mp4"],
+  {
+    resolution: 32,
+    clickToPlay: true,
+    loop: true,
+    size: 100,
+  },
+  scene
+);
 
-var sounds = {};
-
-var binaryTask0 = assetsManager.addBinaryFileTask("Brodowin0 task", atmoPaths.bell);
-binaryTask0.onSuccess = function (task) {
-  sounds.at00 = new BABYLON.Sound("Brodowin0", task.data, scene, soundReady, { loop: true });
-}
-debugger;
-
-sounds.soundsReady = 0;
-function soundReady() {
-  sounds.soundsReady++;
-  if (sounds.soundsReady === Object.keys(atmoPaths).length - 1) {
-    Object.values(sounds).forEach(item => {
-      item.play();
-      console.log(`${item} played!`)
-    });
-  }
-}
-// Shperical UI Panel
-
+// setup spherical UI
 var panel = new BABYLON.GUI.SpherePanel();
 panel.margin = 5;
 panel.radius = 49.9;
@@ -63,21 +42,46 @@ panel.radius = 49.9;
 manager.addControl(panel);
 panel.linkToTransformNode(anchor);
 
+// Sounds
+
+function playSound(soundStr, volumeInDb) {
+  const playVol = Math.pow(10, volumeInDb/20) || 0.5;
+  const sound = sounds[soundStr];
+  sound.setVolume(playVol)
+  sound.play();
+  console.log(`${soundStr} played!`)
+}
+
+
+var audioPaths = {};
+audioPaths.at00 = "./assets/audio/180507_Brodowin_Test_Huegel_5pm45_cut_stereo.ogg";
+audioPaths.bell = "./assets/audio/Agogo\ Bell.wav";
+
+var sounds = {};
+sounds.at00 = new BABYLON.Sound("Atmo", audioPaths.at00, scene, null, { loop: true, autoplay: true , volume: 0.6});
+sounds.bell = new BABYLON.Sound("Bell", audioPaths.bell, scene, null, { loop: false, autoplay: false });
+
+
 //  add some buttons!
 // TODO: How to position?
 
 var addButton = function() {
   var button = new BABYLON.GUI.HolographicButton("orientation");
   panel.addControl(button);
-  button.text = "Button #" + panel.children.length;
+  button.text = "Bird #" + panel.children.length;
   button.height = 200;
 
-  buttonScaler = 5;
+  buttonScaler = 8;
   button.scaling.x = buttonScaler;
   button.scaling.y = buttonScaler;
   button.scaling.z = buttonScaler;
 
-  button.onPointerClickObservable.add(() => console.log("Clicked!"))
+  button.onPointerClickObservable.add(() => {
+    console.log("Clicked!")
+    // spacialise sound
+    sounds.bell.attachToMesh(button.mesh);
+    playSound("bell", -6)
+  })
   button.onPointerEnterObservable.add(() => console.log("Hovered!"))
   // TODO: doesnt work as expected, out trigges on nex button in
   button.onPointerOutObservable.add(() => console.log("Left!")) 
